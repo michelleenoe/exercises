9ccea0310f1935aacc8eeb9d4ef44ec7a6eac61d
michelleenoe <122521774+michelleenoe@users.noreply.github.com>
2024-02-04 15:59:47 +0100

    added søjle

---
+++ barChart/script.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const list = document.querySelector('ul');
+    const maxValues = 20;
+
+    const generateRandomCustomers = () => Math.floor(Math.random() * 101);
+
+    const updateChart = () => {
+        const customerCount = generateRandomCustomers();
+        const li = document.createElement('li');
+        li.style.setProperty('--height', customerCount);
+        li.style.background = `color-mix(in hsl, red, green ${customerCount}%)`;
+        list.appendChild(li);
+        if (list.children.length > maxValues) {
+            list.removeChild(list.firstElementChild);
+        }
+    };
+
+    setInterval(updateChart, 1000);
+});
