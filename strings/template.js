59cceeaad420cc742f98f04aacd4b5b7a79036e9
michelleenoe <122521774+michelleenoe@users.noreply.github.com>
2024-02-09 09:49:36 +0100

    string øvelse

---
+++ strings/template.js
@@ -0,0 +1,38 @@
+// const firstName = "Peter";
+// const catName = "Mandu";
+
+// console.log(`My name is ${firstName}. \nAnd i have a cat called ${catName}`);
+
+// const name = "Peter";
+// const len = name.length;
+// const letter = name [0];
+// console.log(`The first letter of ${name} is ${letter}`);
+
+// console.log(`${name} is ${len} characters long`);
+
+// const name = "Albus Percival Wulfric Brian Dumbledore";
+
+// console.log(name.indexOf ("D"));
+// console.log(name.indexOf (" "));
+
+
+// const fullName = "Albus Percival Wulfric Brian Dumbledore";
+// console.log(`_${fullName.substring(29)}_`);
+
+// const fullName = "Albus Percival Wulfric Brian Dumbledore";
+// const firstName = fullName.substring(15,22)
+// console.log(firstName + "_");
+
+
+// const fullName = "Albus Percival Wulfric Brian Dumbledore";
+// const firstName = fullName.substring(15,23)
+// console.log(firstName + "_");
+
+// const fullName = "Albus Percival Wulfric Brian Dumbledore";
+// const firstName = fullName.substring(25,28)
+// console.log(firstName + "_");
+
+// const firstName = fullName.substring(2,5)
+// console.log(firstName + "_");
+
+
