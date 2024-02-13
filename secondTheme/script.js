2308ae53eaf25e9fd49f2fd08405707010977797
michelleenoe <122521774+michelleenoe@users.noreply.github.com>
2024-02-13 10:44:28 +0100

    ok

--- secondTheme/script.js
+++ secondTheme/script.js
@@ -2,12 +2,17 @@ document.querySelector("select").addEventListener("change", selectChange);
 let currentCol = "orange";
 
 function selectChange(evt) {
-  document.querySelector("body").addEventListener("animationEnd", colorAniEnd)
+
+  currentCol = evt.target.value;
+
+  document.querySelector("body").addEventListener("animationend", colorAniEnd);
   console.log("change", evt.target.value);
-  document.querySelector("body").style.setProperty("--bgcolor", evt.target.value);
+  document.querySelector("body").style.setProperty("--bgcolor", currentCol);
   document.querySelector("body").classList.add("ani");
 }
 
 function colorAniEnd() {
   document.querySelector("body").classList.remove("ani");
+  document.querySelector("body").style.backgroundColor = currentCol;
 }
+
